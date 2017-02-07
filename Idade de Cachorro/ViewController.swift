578880e58274ab9197104414e575c9a1a5b0b897
Douglas Cunha <douglascunha@outlook.com>
2017-02-07 20:17:15 -0200

    Projeto introdutório para entender o funcionamento do processo de desenvolvimento de aplicativos utilizando swift 3 e a ide xcode.

--- Idade de Cachorro/ViewController.swift
+++ Idade de Cachorro/ViewController.swift
@@ -15,6 +15,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    @IBOutlet weak var resultadoCalculo: UILabel!
+    
+    @IBAction func calcularIdade(_ sender: Any) {
+        
+        var idadeCachorro = Int( campoIdadeCachorro.text! )!
+        idadeCachorro = idadeCachorro * 7
+        
+        resultadoCalculo.text = "Ual! O seu cachorro tem " + String( idadeCachorro )+" anos!"
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
